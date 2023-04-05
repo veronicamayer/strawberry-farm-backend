580e1f10cd0158136a537b74420cc3809b4c5b20
veronicamayer <mayerveronica.vm@gmail.com>
2023-04-05 15:12:56 +0100

    basics done, need to move variables into JSON file

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 1818;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let utilization = 0;
+let maxCapacity = 100;
+let price = 0.3;
+let revenue = 0;
+
+app.get("/", (req, res) => {
+    res.json({
+        utilization,
+        maxCapacity,
+        price,
+        revenue,
+    });
+});
+
+app.post("/update-utilization", (req, res) => {
+    const { increment } = req.body;
+    utilization += increment;
+    res.json({ message: "Utilization updated successfully" });
+});
+
+app.post("/sell-all", (req, res) => {
+    revenue = revenue + req.body.utilization * price; // calculate the revenue
+    utilization = 0; // reset the utilization to 0
+    res.json({ revenue }); // send the revenue back to the client
+});
+
+app.post("/reset-revenue", (req, res) => {
+    revenue = 0;
+    res.json({ revenue });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
