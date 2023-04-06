8f31cddd704cbfb538fd0d336a143656319a4441
veronicamayer <mayerveronica.vm@gmail.com>
2023-04-06 16:18:35 +0100

    Revert "moved variables to data.json file and used async/await to read and write the data"
    This reverts commit e2c4e9f5417befdbfe8ac963143373f0ed921fca.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const fs = require("fs/promises");
 
 const app = express();
 const PORT = 1818;
@@ -9,32 +8,10 @@ const PORT = 1818;
 app.use(cors());
 app.use(bodyParser.json());
 
-try {
-    const data = await fs.promises.readFile("data.json");
-    const parsedData = JSON.parse(data);
-    utilization = parsedData.utilization;
-    maxCapacity = parsedData.maxCapacity;
-    price = parsedData.price;
-    revenue = parsedData.revenue;
-    console.log("Data loaded successfully");
-} catch (error) {
-    console.log(error);
-}
-
-const saveData = async () => {
-    try {
-        const data = JSON.stringify({
-            utilization,
-            maxCapacity,
-            price,
-            revenue,
-        });
-        await fs.writeFile("data.json", data);
-        console.log("Data saved successfully");
-    } catch (error) {
-        console.log("Error saving data: ", error);
-    }
-};
+let utilization = 0;
+let maxCapacity = 100;
+let price = 0.3;
+let revenue = 0;
 
 app.get("/", (req, res) => {
     res.json({
@@ -48,20 +25,17 @@ app.get("/", (req, res) => {
 app.post("/update-utilization", (req, res) => {
     const { increment } = req.body;
     utilization += increment;
-    saveData();
     res.json({ message: "Utilization updated successfully" });
 });
 
 app.post("/sell-all", (req, res) => {
     revenue = revenue + req.body.utilization * price; // calculate the revenue
-    utilization = 0; // reset the utilization to 0;
-    saveData();
+    utilization = 0; // reset the utilization to 0
     res.json({ revenue }); // send the revenue back to the client
 });
 
 app.post("/reset-revenue", (req, res) => {
     revenue = 0;
-    saveData();
     res.json({ revenue });
 });
 
